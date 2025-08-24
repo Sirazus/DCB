7b995c74afb6b072ef4bf79fb7af1b9a75919619
Sirazus <Sirazus@users.noreply.github.com>
2025-08-24 17:02:49 +0000

    deploy: 796bcb1f9cd792ba7ee0bc0a2728f214090f4f70

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Este es el contenido de tu nuevo archivo: firebase-config.js
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDA68yp5wR79_famYoft6CZnoHl2GaXRoQ",
+  authDomain: "orischtb.firebaseapp.com",
+  projectId: "orischtb",
+  storageBucket: "orischtb.firebasestorage.app",
+  messagingSenderId: "1066352768294",
+  appId: "1:1066352768294:web:02a02816d69bd07b51fde7",
+  measurementId: "G-E7SVZSLPED" 
+};
+
