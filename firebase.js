614f9634b7a0f0d2bbad6e278a0e5fdf078b1c89
Sirazus <122523858+Sirazus@users.noreply.github.com>
2025-08-24 16:07:13 +0200

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,13 @@
+// Este es el contenido de tu nuevo archivo: firebase-config.js
+
+const firebaseConfig = {
+  apiKey: "%FIREBASE_API_KEY%",
+  authDomain: "%FIREBASE_AUTH_DOMAIN%",
+  projectId: "%FIREBASE_PROJECT_ID%",
+  storageBucket: "%FIREBASE_STORAGE_BUCKET%",
+  messagingSenderId: "%FIREBASE_MESSAGING_SENDER_ID%",
+  appId: "%FIREBASE_APP_ID%",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
